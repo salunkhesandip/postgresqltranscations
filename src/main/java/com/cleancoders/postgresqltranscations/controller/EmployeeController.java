54daa0ab98d0331821e6038c0382cf2b4663a5c1
Sandip Salunkhe <sandip.v.salunkhe@gmail.com>
2023-09-24 21:24:27 -0500

    Add Patch request

--- src/main/java/com/cleancoders/postgresqltranscations/controller/EmployeeController.java
+++ src/main/java/com/cleancoders/postgresqltranscations/controller/EmployeeController.java
@@ -2,6 +2,9 @@ package com.cleancoders.postgresqltranscations.controller;
 
 import com.cleancoders.postgresqltranscations.dto.EmployeeDTO;
 import com.cleancoders.postgresqltranscations.service.EmployeeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -9,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -65,5 +69,21 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDTO> updateEmployee(@Valid @RequestBody EmployeeDTO employeeDTO) {
         EmployeeDTO updatedEmployee = employeeService.updateEmployee(employeeDTO);
         return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @Operation(description = "Partial Update/Patch Employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee patched successfully")
+    })
+    @PatchMapping(value = "/{id}", consumes = "application/json-patch+json")
+    public ResponseEntity<EmployeeDTO> patchEmployee(@PathVariable("id") Long id,
+                                                     @RequestBody JsonPatch jsonPatchRequest) {
+        EmployeeDTO patchedEmployee;
+        try {
+            patchedEmployee = employeeService.patchEmployee(id, jsonPatchRequest);
+        } catch (JsonPatchException | JsonProcessingException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok(patchedEmployee);
     }
 }
--- src/main/java/com/cleancoders/postgresqltranscations/service/EmployeeService.java
+++ src/main/java/com/cleancoders/postgresqltranscations/service/EmployeeService.java
@@ -6,6 +6,11 @@ import com.cleancoders.postgresqltranscations.exception.EmployeeConflictExceptio
 import com.cleancoders.postgresqltranscations.exception.EmployeeNotFoundException;
 import com.cleancoders.postgresqltranscations.mapper.EmployeeMapper;
 import com.cleancoders.postgresqltranscations.repository.EmployeeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +22,7 @@ import java.util.Optional;
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper mapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public EmployeeService(EmployeeRepository employeeRepository, EmployeeMapper mapper) {
         this.employeeRepository = employeeRepository;
@@ -55,5 +61,17 @@ public class EmployeeService {
         } else {
             throw new EmployeeNotFoundException("Employee " + employeeDTO.getEmpId() + " not found");
         }
+    }
+
+    public EmployeeDTO patchEmployee(Long id, JsonPatch jsonPatchRequest)
+            throws JsonPatchException, JsonProcessingException {
+        EmployeeDTO employeeDTO = findEmployee(id);
+        return updateEmployee(applyPatchToEmployee(jsonPatchRequest, employeeDTO));
+    }
+
+    private EmployeeDTO applyPatchToEmployee(JsonPatch patch, EmployeeDTO employeeDTO)
+            throws JsonPatchException, JsonProcessingException {
+        JsonNode patched = patch.apply(objectMapper.convertValue(employeeDTO, JsonNode.class));
+        return objectMapper.treeToValue(patched, EmployeeDTO.class);
     }
 }
