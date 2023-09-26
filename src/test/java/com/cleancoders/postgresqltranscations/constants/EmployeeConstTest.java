3a7652255c1d15f5280c7bc3037ceb2353f8c846
Sandip Salunkhe <sandip.v.salunkhe@gmail.com>
2023-09-25 22:23:40 -0500

    Add MockMvc Rest controller test for get employee endpoint

---
+++ src/test/java/com/cleancoders/postgresqltranscations/constants/EmployeeConstTest.java
@@ -0,0 +1,7 @@
+package com.cleancoders.postgresqltranscations.constants;
+
+public class EmployeeConstTest {
+    public static final Long TEST_EMPLOYEE_ID = 123L;
+    public static final String TEST_EMPLOYEE_NAME = "Test MVC";
+    public static final Long TEST_EMPLOYEE_SALARY = 100000L;
+}
---
+++ src/test/java/com/cleancoders/postgresqltranscations/controller/EmployeeControllerTest.java
@@ -0,0 +1,41 @@
+package com.cleancoders.postgresqltranscations.controller;
+
+import com.cleancoders.postgresqltranscations.exception.EmployeeNotFoundException;
+import com.cleancoders.postgresqltranscations.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.cleancoders.postgresqltranscations.constants.EmployeeConstTest.TEST_EMPLOYEE_ID;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EmployeeController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EmployeeControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    EmployeeService employeeService;
+
+    //Given_Precondition_When_StateUnderTest_Then_ExpectedBehavior
+
+    @Test
+    void Given_EmployeeId_When_GetEmployee_Then_SuccessResponse() throws Exception {
+        mockMvc.perform(get("/employees/" + TEST_EMPLOYEE_ID))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void Given_EmployeeId_When_GetEmployee_Then_FailureResponse() throws Exception {
+        given(employeeService.findEmployee(anyLong())).willThrow(new EmployeeNotFoundException());
+        mockMvc.perform(get("/employees/" + TEST_EMPLOYEE_ID))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
